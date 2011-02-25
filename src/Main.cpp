a5ab71383a9282fcd5cbd96a09bb91231cf317b8
Nicolas Bigaouette <nbigaouette@gmail.com>
2011-02-24 20:39:39 -0500

    Print different type size

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,12 +5,26 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <vector>
+#include <stdint.h> // uint64_t
 
 // **************************************************************
 int main(int argc, char *argv[])
 {
-
     std::cout << "Hello world!" << std::endl;
+    std::cout << "sizeof(int) = " << sizeof(int) << std::endl;
+    std::cout << "sizeof(long int) = " << sizeof(long int) << std::endl;
+    std::cout << "sizeof(long long int) = " << sizeof(long long int) << std::endl;
+    std::cout << "sizeof(unsigned int) = " << sizeof(unsigned int) << std::endl;
+    std::cout << "sizeof(unsigned long int) = " << sizeof(unsigned long int) << std::endl;
+    std::cout << "sizeof(unsigned long long int) = " << sizeof(unsigned long long int) << std::endl;
+    std::cout << "sizeof(uint64_t) = " << sizeof(uint64_t) << std::endl;
+    std::cout << "sizeof(uint32_t) = " << sizeof(uint32_t) << std::endl;
+    std::cout << "sizeof(int64_t) = " << sizeof(int64_t) << std::endl;
+    std::cout << "sizeof(int32_t) = " << sizeof(int32_t) << std::endl;
+    std::cout << "sizeof(std::vector<uint64_t>::size_type) = " << sizeof(std::vector<uint64_t>::size_type) << std::endl;
+    std::cout << "sizeof(std::vector<double>::size_type) = " << sizeof(std::vector<double>::size_type) << std::endl;
+    std::cout << "sizeof(std::vector<float>::size_type) = " << sizeof(std::vector<float>::size_type) << std::endl;
 
     return EXIT_SUCCESS;
 }
